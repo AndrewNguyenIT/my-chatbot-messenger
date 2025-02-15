85c9e9a4bb19ed8c9016e03a840b664ad4316e12
Nhut Nguyen <nhutb2205896@student.ctu.edu.vn>
2025-02-15 18:39:57 +0700

    first commit

--- src/server.js
+++ src/server.js
@@ -5,16 +5,20 @@ import webRoutes from "./route/web";
 
 let app = express();
 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 viewEngine(app);
 
 
 webRoutes(app);
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 let port = process.env.PORT || 8080;
 
-app .listen(port, () => {
+app.listen(port, () => {
     console.log("App is running at the port:" + port);
 })
